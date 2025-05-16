50a54b4a91d8dccd689d83013b61e49512793870
neel-p <63221970+neel-p@users.noreply.github.com>
2025-05-16 09:33:12 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,32 @@
+//  @type {import('next').NextConfig}
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  analyzerMode: "static",
+  reportFilename: "./analyze/bundle-report.html", // customize if needed
+  openAnalyzer: false, // don't open browser on build
+});
+
+const nextConfig = {
+  reactStrictMode: false,
+  // Enable page-level static optimization
+  output: "standalone",
+  staticPageGenerationTimeout: 180,
+  // Enable image optimization
+  images: {
+    domains: [],
+    unoptimized: false,
+  },
+  // Enable compression
+  compress: true,
+  // Enable production source maps
+  productionBrowserSourceMaps: false,
+  // Enable page-level static optimization
+  poweredByHeader: false,
+  // Enable page-level static optimization
+  generateEtags: true,
+};
+
+export default withBundleAnalyzer(nextConfig);
