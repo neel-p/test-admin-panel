a0696e0c9ee604131775383cd3320edf5fbc9732
neel-p <63221970+neel-p@users.noreply.github.com>
2025-05-16 10:30:07 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,29 @@
-//  @type {import('next').NextConfig}
+// @type {import('next').NextConfig}
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-  analyzerMode: "static",
-  reportFilename: "./analyze/bundle-report.html", // customize if needed
-  openAnalyzer: false, // don't open browser on build
-});
+const isAnalyze = process.env.ANALYZE === "true";
+
+const withBundleAnalyzer = isAnalyze
+  ? require("@next/bundle-analyzer")({
+      enabled: true,
+      analyzerMode: "static",
+      reportFilename: "./analyze/bundle-report.html",
+      openAnalyzer: false,
+    })
+  : (config) => config;
 
 const nextConfig = {
   reactStrictMode: false,
-  // Enable page-level static optimization
   output: "standalone",
   staticPageGenerationTimeout: 180,
-  // Enable image optimization
   images: {
     domains: [],
     unoptimized: false,
   },
-  experimental: {
-    appDir: true,
-  },
-  // Enable compression
   compress: true,
-  // Enable production source maps
   productionBrowserSourceMaps: false,
-  // Enable page-level static optimization
   poweredByHeader: false,
-  // Enable page-level static optimization
   generateEtags: true,
 };
 
